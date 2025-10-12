9404230d363fc7278d47cf04a452e1074c5a3ae6
Anton <volodin.anton003@gmail.com>
2025-10-12 19:01:41 +0300

    homework_10_2 and another changes

--- src/processing.py
+++ src/processing.py
@@ -14,12 +14,11 @@ def filter_by_state (dictionnary_list: list[dict[str, str | int]], state) -> lis
 filter_by_state (dictionnary_list, state = 'EXECUTED')
 
 
-
 def sort_by_date(dictionnary_list: list[dict[str, str | int]], ascending=False) -> list[dict[str, str | int]]:
     """Сортировка по дате"""
 
     sorted_list = sorted(dictionnary_list, key=lambda list_: list_.get('date', 0), reverse=True)
     return sorted_list
 
-#sorted_new_list = sort_by_date(dictionnary_list)
-#print(sorted_new_list)
+sorted_new_list = sort_by_date(dictionnary_list)
+print(sorted_new_list)
